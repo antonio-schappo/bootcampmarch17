dc48c98cb9cb3d69c3f9783877cf448f1d55e2e2
Antônio Schappo <antonio.schappo@mercadolivre.com>
2022-03-17 17:19:24 -0300

    Pratica Integradora 1 de Java + Spring I feita

---
+++ src/main/java/com/example/exerciseone/ControllerRomanos.java
@@ -0,0 +1,28 @@
+package com.example.exerciseone;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerRomanos {
+
+    @GetMapping("/romanos")
+    public String controllerMethod(@RequestParam int numero) {
+        return romanos(numero);
+    }
+
+    public static String romanos(int num){
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanLiterals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder roman = new StringBuilder();
+        for(int i=0;i<values.length;i++) {
+            while(num >= values[i]) {
+                num -= values[i];
+                roman.append(romanLiterals[i]);
+            }
+        }
+        return roman.toString();
+    }
+}
